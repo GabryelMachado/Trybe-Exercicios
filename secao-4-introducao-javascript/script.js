302c9b3bd3246f0a794060d07ef9106be7639034
GabryelMachado <gabryelmachado.japah@gmail.com>
2022-09-22 00:24:27 -0300

    Exercicio6 if/else

---
+++ secao-4-introducao-javascript/script.js
@@ -0,0 +1,14 @@
+const a = 45;
+const b = 33;
+let resultado;
+
+if (a >= 33) {
+    resultado = 45;
+}
+else if (a <= 33) {
+    resultado = 33;
+}
+else {
+    resultado = 'Inesperado'
+}
+console.log(resultado)
--- secao-4-introducao-javascript/scripts.js
+++
@@ -1,4 +0,0 @@
-const a = 77
-const b = 53
-
-console.log(a%b)
