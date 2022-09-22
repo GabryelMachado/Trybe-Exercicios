c721a7559285a2e089f41a138935c62c67ac8761
GabryelMachado <gabryelmachado.japah@gmail.com>
2022-09-22 00:31:29 -0300

    Exercicio7 if/else

--- secao-4-introducao-javascript/script.js
+++ secao-4-introducao-javascript/script.js
@@ -1,12 +1,16 @@
 const a = 45;
 const b = 33;
+const c = 88;
 let resultado;
 
-if (a >= 33) {
+if (a >= 33 && a >= 88) {
     resultado = 45;
 }
-else if (a <= 33) {
+else if (b >= 45 && b >= 88) {
     resultado = 33;
+}
+else if(c >= 45 && c >= 33) {
+    resultado = 88
 }
 else {
     resultado = 'Inesperado'
