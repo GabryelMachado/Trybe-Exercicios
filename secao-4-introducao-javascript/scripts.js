639089b934ee7788b0dd27ffb9236fb7ec3c4349
GabryelMachado <gabryelmachado.japah@gmail.com>
2022-09-22 00:06:10 -0300

    Exercicio1 adição

--- secao-4-introducao-javascript/scripts.js
+++ secao-4-introducao-javascript/scripts.js
@@ -1,17 +1,4 @@
-let pessoaCandidata = 'lista';
+const a = 77
+const b = 53
 
-switch (pessoaCandidata) {
-    case 'aprovada':
-      console.log('Parabéns, você foi aprovado')
-      break;
-
-    case 'lista':
-      console.log('Você está na nossa fila de espera')
-      break;
-
-    case 'reprovada':
-      console.log('Você foi reprovado')
-      break;
-
-    default:('Não atingiu pontuação minima para análise')
-}
+console.log(a+b)
