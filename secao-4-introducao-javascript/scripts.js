4b05594ebc042431681c531734196c49b30dfe66
GabryelMachado <gabryelmachado.japah@gmail.com>
2022-09-21 23:58:15 -0300

    Para fixar switch/case

--- secao-4-introducao-javascript/scripts.js
+++ secao-4-introducao-javascript/scripts.js
@@ -1,14 +1,17 @@
-let pessoaCandidata = 85;
+let pessoaCandidata = 'lista';
 
-if (pessoaCandidata >= 80) {
-    console.log("Parabéns, você foi aprovado(a)");
-}
-else if (pessoaCandidata < 80 && pessoaCandidata >= 60) { 
-    console.log("Você está na nossa lista de espera");
-}
-else if (pessoaCandidata < 60){
-    console.log("Você foi reprovado");
-}
-else {
-    console.log("Tente novamente em 06 meses");
+switch (pessoaCandidata) {
+    case 'aprovada':
+      console.log('Parabéns, você foi aprovado')
+      break;
+
+    case 'lista':
+      console.log('Você está na nossa fila de espera')
+      break;
+
+    case 'reprovada':
+      console.log('Você foi reprovado')
+      break;
+
+    default:('Não atingiu pontuação minima para análise')
 }
