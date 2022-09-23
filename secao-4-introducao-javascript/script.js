7b225e5301f89b2e216a3e106c8612c8793fd0af
GabryelMachado <gabryelmachado.japah@gmail.com>
2022-09-23 18:56:11 -0300

    Resolve fatorial de 10

--- secao-4-introducao-javascript/script.js
+++ secao-4-introducao-javascript/script.js
@@ -1,14 +1,7 @@
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let resultado = 0
+let numero = 10
+let resultado = 1
 
-for (let i = 0; i < numbers.length; i += 1 ) {
-    resultado += numbers[i]
+for (let i = 1; i <= numero; i += 1){
+    resultado *= i
 }
-console.log(resultado / numbers.length);
-
-if (resultado / numbers.length > 20) {
-    console.log('Valor maior que 20')
-}
-else {
-    console.log('Valor menor que 20')
-}
+console.log(resultado)
